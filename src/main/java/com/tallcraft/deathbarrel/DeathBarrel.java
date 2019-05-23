66f118002297d2daf684c48c48be60580d4b39b4
Trikolon <git@pbz.im>
2019-05-23 21:54:16 +0200

    Added placeBarrel method which makes barrel placing cancellable by third party plugins

--- src/main/java/com/tallcraft/deathbarrel/DeathBarrel.java
+++ src/main/java/com/tallcraft/deathbarrel/DeathBarrel.java
@@ -1,8 +1,7 @@
 package com.tallcraft.deathbarrel;
 
-import java.util.List;
-
 import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Barrel;
@@ -10,10 +9,14 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class DeathBarrel extends JavaPlugin implements Listener {
 
@@ -22,6 +25,30 @@ public final class DeathBarrel extends JavaPlugin implements Listener {
         // All you have to do is adding this line in your onEnable method:
         Metrics metrics = new Metrics(this);
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+
+    /**
+     * Places a barrel block while respecting other plugins / player build permission
+     *
+     * @param player   - Player to act as.
+     * @param location - Where to place the barrel block
+     * @return The barrel block placed or null on failure
+     */
+    private Barrel placeBarrel(Player player, Location location) {
+        Block block = location.getBlock();
+        Block blockBelow = location.clone().subtract(0, 1, 0).getBlock();
+        ItemStack item = new ItemStack(Material.BARREL, 1);
+
+        BlockPlaceEvent blockPlaceEvent = new BlockPlaceEvent(block, block.getState(), blockBelow, item, player, true, EquipmentSlot.HAND);
+        Bukkit.getServer().getPluginManager().callEvent(blockPlaceEvent);
+
+        if (blockPlaceEvent.isCancelled()) {
+            return null;
+        }
+
+        block.setType(Material.BARREL);
+        return (Barrel) block.getState();
     }
 
 
@@ -35,21 +62,23 @@ public final class DeathBarrel extends JavaPlugin implements Listener {
 
         Location location = player.getLocation();
         List<ItemStack> drops = event.getDrops();
-
         // Player died with empty inventory
-        if(drops.size() == 0) {
+        if (drops.size() == 0) {
             return;
         }
 
-        Block bottomBlock = location.getBlock();
-        bottomBlock.setType(Material.BARREL);
-        Barrel bottomBarrel = (Barrel) bottomBlock.getState();
+        Barrel bottomBarrel = placeBarrel(player, location);
+        if (bottomBarrel == null) {
+            // Placing the barrel failed (something cancelled the block place event)
+            return;
+        }
+
         Inventory bottomBarrelInventory = bottomBarrel.getInventory();
 
         int dropSize = drops.size();
         int barrelCapacity = bottomBarrelInventory.getSize();
 
-        if(barrelCapacity >= dropSize) {
+        if (barrelCapacity >= dropSize) {
             bottomBarrelInventory.setContents(drops.toArray(new ItemStack[0]));
         } else {
             // Drops dont fit in one barrel, lets overflow into a second one
@@ -58,9 +87,11 @@ public final class DeathBarrel extends JavaPlugin implements Listener {
             bottomBarrelInventory.setContents(drops.subList(0, barrelCapacity).toArray(new ItemStack[0]));
 
             // Fill top barrel
-            Block topBlock = location.add(0, 1, 0).getBlock();
-            topBlock.setType(Material.BARREL);
-            Barrel topBarrel = (Barrel) topBlock.getState();
+            Barrel topBarrel = placeBarrel(player, location.add(0, 1, 0));
+            if (topBarrel == null) {
+                // Placing the barrel failed (something cancelled the block place event)
+                return;
+            }
             Inventory topBarrelInventory = topBarrel.getInventory();
             topBarrelInventory.setContents(drops.subList(barrelCapacity, dropSize).toArray(new ItemStack[0]));
         }
